da058c4ab5142958402772fb3321c39d264635c7
virusas3000 <your@email.com>
2026-05-18 16:06:08 +0800

    Remove hardcoded Azure key - use env var only

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import whisper
 from gtts import gTTS
 import azure.cognitiveservices.speech as speechsdk
 
-AZURE_KEY = os.environ.get("AZURE_SPEECH_KEY", "REDACTED_AZURE_KEY")
+AZURE_KEY = os.environ.get("AZURE_SPEECH_KEY", "")
 AZURE_REGION = os.environ.get("AZURE_SPEECH_REGION", "eastasia")
 
 app = Flask(__name__)
